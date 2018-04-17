5680a05a30762389050452ab4e27f5cbe8de7126
gloriafecu <gloriafercu@gmail.com>
2018-04-17 17:04:12 +0200

    add functionality js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 'use strict';
 
-var buttonAdd = document.querySelector('.add');
-var list1 = document.querySelector('.list1');
-var list2 = document.querySelector('.list2');
+var buttonAdd    = document.querySelector('.add');
+var buttonRemove = document.querySelector('.remove');
+var list1        = document.querySelector('.list1');
+var list2        = document.querySelector('.list2');
 
 
 function addUserList() {
-list2.innerHTML = list1.innerHTML;
-list1.innerHTML = '';
+    list2.innerHTML = list1.innerHTML;
+    list1.innerHTML = '';
 }
 buttonAdd.addEventListener('click', addUserList);
+
+function removeUserList() {
+    list1.innerHTML = list2.innerHTML;
+    list2.innerHTML = '';
+}
+buttonRemove.addEventListener('click', removeUserList);
